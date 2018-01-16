a52dbae41661b248d9899dc7c913414b6e2aa104
Matt Crook <mcro591@aucklanduni.ac.nz>
2018-01-16 20:43:26 +1300

    create react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import GameChooser from './components/GameChooser';
+
+class App extends Component {
+  render() {
+    return <GameChooser />;
+  }
+}
+
+export default App;
---
+++ src/components/GameChooser.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class GameChooser extends Component {
+  render() {
+    return (
+      <ul>
+        <li>ME 1</li>
+        <li>ME 2</li>
+        <li>ME 3</li>
+      </ul>
+    );
+  }
+}
+
+export default GameChooser;
