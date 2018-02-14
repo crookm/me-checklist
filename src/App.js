ea57075470c73e10b406fe78e8fbeb80967ec26e
Matt Crook <mcro591@aucklanduni.ac.nz>
2018-02-14 19:15:31 +1300

    save user data in browser

--- src/App.js
+++ src/App.js
@@ -28,7 +28,24 @@ class App extends Component {
 
     items[key].completion = toggled;
 
+    if (typeof(Storage) !== 'undefined'){
+      window.localStorage[game] = JSON.stringify(items.map(i => i.completion));
+    }
+
     set(items); // return the new items so caller can update state
+  }
+
+  loadUserData(game, def, set) {
+    if (typeof(Storage) !== 'undefined'){
+      if (typeof(window.localStorage[game]) === 'string') {
+        let data = JSON.parse(window.localStorage[game]);
+        set(def.map((item, i) => {
+          item.completion = data[i];
+          item.completion.datetime = new Date(item.completion.datetime);
+          return item;
+        }));
+      }
+    }
   }
 
   render() {
@@ -36,9 +53,9 @@ class App extends Component {
       <Router>
         <Switch>
           <Route path="/" exact component={GameChooser} />
-          <Route path="/one" render={props => <ME1 {...props} handleToggle={this.handleToggle} />} />
-          <Route path="/two" render={props => <ME2 {...props} handleToggle={this.handleToggle} />} />
-          <Route path="/three" render={props => <ME3 {...props} handleToggle={this.handleToggle} />} />
+          <Route path="/one" render={props => <ME1 {...props} handleToggle={this.handleToggle} loadUserData={this.loadUserData} />} />
+          <Route path="/two" render={props => <ME2 {...props} handleToggle={this.handleToggle} loadUserData={this.loadUserData} />} />
+          <Route path="/three" render={props => <ME3 {...props} handleToggle={this.handleToggle} loadUserData={this.loadUserData} />} />
           <Route component={NotFound} />
         </Switch>
       </Router>
--- src/_pages/ME1.js
+++ src/_pages/ME1.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import Checklist from '../_components/Checklist';
 
+const GAME = 'one';
+
 class ME1 extends Component {
   constructor(props) {
     super(props);
@@ -35,15 +37,21 @@ class ME1 extends Component {
     };
     this.toggleCompleted = this.toggleCompleted.bind(this);
   }
+  
+  componentDidMount() {
+    this.props.loadUserData(GAME, this.state.items, updated_items => {
+      this.setState({ items: updated_items })
+    });
+  }
 
   toggleCompleted(key) {
-    this.props.handleToggle('one', key, this.state.items, new_items => {
+    this.props.handleToggle(GAME, key, this.state.items, new_items => {
       this.setState({ items: new_items })
     });
   }
 
   render() {
-    return <Checklist items={this.state.items} onToggle={this.toggleCompleted} />;
+    return (<Checklist items={this.state.items} onToggle={this.toggleCompleted} />);
   }
 }
 
