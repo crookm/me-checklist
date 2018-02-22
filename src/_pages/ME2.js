0ea22ff6ef51341a50c6fa059709d132f0853b31
Matt Crook <mcro591@aucklanduni.ac.nz>
2018-02-23 11:16:33 +1300

    flesh-out the other two games

--- src/_pages/ME2.js
+++ src/_pages/ME2.js
@@ -1,8 +1,44 @@
 import React, { Component } from "react";
 
+import Checklist from "../_components/Checklist";
+import LogoME2 from "../_assets/LogoME2";
+
+import ME2List from "../_assets/ME2List";
+
+const GAME = "two";
+
 class ME2 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: ME2List
+    };
+    this.toggleCompleted = this.toggleCompleted.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.loadUserData(GAME, this.state.items, updated_items => {
+      this.setState({ items: updated_items });
+    });
+  }
+
+  toggleCompleted(key) {
+    this.props.handleToggle(GAME, key, this.state.items, new_items => {
+      this.setState({ items: new_items });
+    });
+  }
+
   render() {
-    return <p>me 2</p>;
+    return (
+      <div className="row align-center">
+        <div className="columns medium-6">
+          <LogoME2
+            style={{ width: "250px", margin: "50px auto", display: "block" }}
+          />
+          <Checklist items={this.state.items} onToggle={this.toggleCompleted} />
+        </div>
+      </div>
+    );
   }
 }
 
--- src/_pages/ME3.js
+++ src/_pages/ME3.js
@@ -1,8 +1,44 @@
 import React, { Component } from "react";
 
+import Checklist from "../_components/Checklist";
+import LogoME3 from "../_assets/LogoME3";
+
+import ME3List from "../_assets/ME3List";
+
+const GAME = "three";
+
 class ME3 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: ME3List
+    };
+    this.toggleCompleted = this.toggleCompleted.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.loadUserData(GAME, this.state.items, updated_items => {
+      this.setState({ items: updated_items });
+    });
+  }
+
+  toggleCompleted(key) {
+    this.props.handleToggle(GAME, key, this.state.items, new_items => {
+      this.setState({ items: new_items });
+    });
+  }
+
   render() {
-    return <p>me 3</p>;
+    return (
+      <div className="row align-center">
+        <div className="columns medium-6">
+          <LogoME3
+            style={{ width: "250px", margin: "50px auto", display: "block" }}
+          />
+          <Checklist items={this.state.items} onToggle={this.toggleCompleted} />
+        </div>
+      </div>
+    );
   }
 }
 
