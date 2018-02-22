e56a5173d3665f374cab281fdae99bbe823b0189
Matt Crook <mcro591@aucklanduni.ac.nz>
2018-02-23 11:02:26 +1300

    toggleable info box

--- src/_components/Checklist.js
+++ src/_components/Checklist.js
@@ -2,6 +2,16 @@ import React, { Component } from "react";
 import "../_styles/Checklist.css";
 
 class Checklist extends Component {
+  toggleCarat(index) {
+    window.$(`.i-${index}`).toggleClass("active");
+    window.$(`.i-${index} .carat`).toggleClass("down");
+    window.$(`.i-${index} .info`).slideToggle();
+  }
+
+  componentDidMount() {
+    window.$(".info").hide();
+  }
+
   render() {
     return (
       <div className="checklist">
@@ -19,13 +29,35 @@ class Checklist extends Component {
                 </label>
               </div>
               <div className="columns title">
-                <p>{item.title}</p>
+                <p>
+                  <a onClick={e => this.props.onToggle(index, e)}>
+                    {item.title}
+                  </a>
+                </p>
               </div>
               <div className="columns shrink">
-                <div className="carat-down" />
+                <a onClick={e => this.toggleCarat(index, e)}>
+                  <div className="carat down up" />
+                </a>
               </div>
             </div>
-            <div className="row info">the stuff</div>
+            <div className="row info">
+              <div className="columns small-12">
+                <h3>{item.title}</h3>
+                <p>{item.desc}</p>
+
+                <div className="row timeline">
+                  <div className="columns medium-6">
+                    <h4>Complete after</h4>
+                    <p>{item.timeline.after}</p>
+                  </div>
+                  <div className="columns medium-6">
+                    <h4>Complete before</h4>
+                    <p>{item.timeline.before}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         ))}
       </div>
