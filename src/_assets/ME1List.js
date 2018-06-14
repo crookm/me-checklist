ff86b51e0cbb95a76c876c49fdb2fd70dbe0474b
Matt Crook <mcro591@aucklanduni.ac.nz>
2018-06-14 15:06:22 +1200

    data for like half of the first game

--- src/_assets/ME1List.js
+++ src/_assets/ME1List.js
@@ -1,10 +1,36 @@
 export default {
   0: {
-    title: "somethng",
-    desc: "a longer description of what you need to do",
+    title: `<b>Prologue</b>: Find the Beacon`,
+    desc: `You are in charge of the ground team dropped onto Eden Prime, an Alliance colony under attack. Your primary goal is to find and secure the Prothean beacon.`,
+    wiki: `http://masseffect.wikia.com/wiki/Prologue:_Find_the_Beacon`,
     timeline: {
-      before: "do before this event",
-      after: "do after this even"
+      before: `Chapter 1: Citadel - Expose Saren`,
+      after: `<i>This is the first mission</i>`
+    },
+    completion: {
+      done: false,
+      datetime: null
+    }
+  },
+  1: {
+    title: `HINT: Charm/intimidate skill`,
+    desc: `Right throughout the game, a high charm or intimidate skill will help you out significantly, and expose you to more story. Balance is not ideal here, stick to one or the other.`,
+    timeline: {
+      before: `Citadel: Asari Consort, or Noveria: Lorik Qui'in`,
+      after: `Prologue`
+    },
+    completion: {
+      done: false,
+      datetime: null
+    }
+  },
+  2: {
+    title: `HINT: Electronics skill`,
+    desc: `Further along the game, a medium electronics skill will provide an easy bypass to a challenge.<br /><br />
+      You can choose to level up this skill on a squadmate, such as <b>Kaidan</b>, instead of using your own points. Remember to bring them on the Noveria missions.`,
+    timeline: {
+      before: `Chapter 4: Noveria - Geth Interest`,
+      after: `Prologue`
     },
     completion: {
       done: false,
@@ -12,15 +38,252 @@ export default {
     }
   },
   10: {
-    title: "something 2 rellly long sdjkf kjsh dkjahdkfjhs",
-    desc: "a longer description of what you need to do",
+    title: `<b>Chapter 1</b>: Citadel - Expose Saren`,
+    desc: `You have to prove to the Citadel Council that Saren, one of their Spectre agents, has gone rogue.<br /><br />
+      Follow the 'Barla Von' route.`,
+    wiki: `http://masseffect.wikia.com/wiki/Citadel:_Expose_Saren`,
     timeline: {
-      before: "do before this event",
-      after: "do after this even"
+      before: `Chapter 2: Finding Liara`,
+      after: `Prologue`
     },
     completion: {
       done: false,
       datetime: null
     }
-  }
+  },
+  11: {
+    title: `HINT: The Barla Von Route`,
+    desc: `When you acquire the mission above, you get the option of talking to Harkin or Barla Von. Talk to the latter, so you will be able to have Wrex as a squadmate.<br /><br />
+      When you acquire Wrex, he will say something about a Turian in the Med Clinic - <b>choose to go to the Med Clinic, rather than deal with Fist.</b>`,
+    timeline: {
+      before: `Chapter 1: Citadel - Expose Saren (before going to Fist)`,
+      after: `Chapter 1: Citadel - Expose Saren (acquisition)`
+    },
+    completion: {
+      done: false,
+      datetime: null
+    }
+  },
+  12: {
+    title: `Citadel: Reporter's Request`,
+    desc: `You found an OSD containing information about Fist's illegal activities. Someone will likely want to get their hands on this information.<br /><br />
+      This 'OSD' is found in Fist's office, found in Chapter 1: Citadel - Expose Saren. You can also return after the mission to get it. Give it to Emily Wong, who will be on the stairs leading to Flux on the upper wards.`,
+    wiki: `http://masseffect.wikia.com/wiki/Citadel:_Reporter%27s_Request`,
+    timeline: {
+      before: `Chapter 2: Finding Liara`,
+      after: `Chapter 1: Citadel - Expose Saren`
+    },
+    completion: {
+      done: false,
+      datetime: null
+    }
+  },
+  13: {
+    title: `Citadel: Asari Consort`,
+    desc: `From the Presidium, cross the bridge and go to the Consort's Chambers, speak to Nelyna. You will ge a session with the consort as you're about to leave. Complete this line of missions. <br /><br />
+      After successfully convincing Septimus, complete <b>Citadel: Xeltan's Complaint</b> before returning to Sha'ira.`,
+    wiki: `http://masseffect.wikia.com/wiki/Citadel:_Asari_Consort`,
+    timeline: {
+      before: `Citadel: Xentan's Complaint`,
+      after: `Chapter 1: Citadel - Expose Saren`
+    },
+    completion: {
+      done: false,
+      datetime: null
+    }
+  },
+  14: {
+    title: `Citadel: Xeltan's Complaint`,
+    desc: `General Septimus agreed to stop spreading rumors about the Asari Consort. He mentioned an elcor named Xeltan, who believed the rumors and has taken the false information to the Elcor Ambassador. Septimus asked you to tell Xeltan the truth.<br /><br />
+      This mission should be completed before returning to Sha'ira, e.g., as soon as it is acquired.`,
+    wiki: `http://masseffect.wikia.com/wiki/Citadel:_Xeltan%27s_Complaint`,
+    timeline: {
+      before: `Citadel: Asari Consort (before completion)`,
+      after: `Citadel: Asari Consort (after acquisition)`
+    },
+    completion: {
+      done: false,
+      datetime: null
+    }
+  },
+  16: {
+    title: `Citadel: Rita's Sister`,
+    desc: `Speak with Rita, a waitress at Flux. She's concerned for her sister, who is working undercover for C-Sec. Rita thinks her sister, Jenna, is in over her head, but she can't convince her to stop.`,
+    wiki: `http://masseffect.wikia.com/wiki/Citadel:_Rita%27s_Sister`,
+    timeline: {
+      before: `Chapter 7: Ilos`,
+      after: `Chapter 1: Citadel - Expose Saren`
+    },
+    completion: {
+      done: false,
+      datetime: null
+    }
+  },
+  17: {
+    title: `HINT: Always talk to Conrad`,
+    desc: `Conrad Verner, located at the top of the stairs in the upper markets on the wards of the Citadel, has three interactions available throughout the game. Handle them however you want, but speaking to him is helpful in future games. He disappears after the end of each dialogue, but keep checking back for him after main missions.`,
+    timeline: {
+      before: `Chapter 7: Ilos`,
+      after: `Prologue`
+    },
+    completion: {
+      done: false,
+      datetime: null
+    }
+  },
+  18: {
+    title: `HINT: Upgrade grenade capacity`,
+    desc: `Visit the shops on the Citadel, and look for any opportunity to upgrade your grenade capacity. You will need them later on.`,
+    timeline: {
+      before: `Chapter 3: Feros - Geth Attack`,
+      after: `Prologue`
+    },
+    completion: {
+      done: false,
+      datetime: null
+    }
+  },
+  19: {
+    title: `Citadel: Doctor Michel`,
+    desc: `Dr. Chloe Michel is being blackmailed. Someone learned about her dismissal from a previous employer and the circumstances surrounding it. Now, if she doesn't give these people her supplies they're threatening to go public with what they know.`,
+    wiki: `http://masseffect.wikia.com/wiki/Citadel:_Doctor_Michel`,
+    timeline: {
+      before: `Chapter 2: Finding Liara`,
+      after: `Citadel: Rita's Sister`
+    },
+    completion: {
+      done: false,
+      datetime: null
+    }
+  },
+  20: {
+    title: `<b>Chapter 2</b>: Finding Liara`,
+    desc: `Liara T'Soni, an asari archeologist famed for her knowledge of the Protheans, is being sought by Saren. Her last known location was somewhere in the Artemis Tau cluster.`,
+    wiki: `http://masseffect.wikia.com/wiki/Find_Liara_T%27Soni`,
+    timeline: {
+      before: `Chapter 3: Feros - Geth Attack`,
+      after: `Citadel: Doctor Michel`
+    },
+    completion: {
+      done: false,
+      datetime: null
+    }
+  },
+  23: {
+    title: `HINT: Speak with your crew`,
+    desc: `After each mission, go through the ship and speak with all of your squadmates. This is how you pick-up their 'loyalty' missions. Focus on Wrex, Tali, and Garrus in particular.`,
+    timeline: {
+      before: `Chapter 7: Ilos`,
+      after: `Chapter 1: Citadel - Expose Saren`
+    },
+    completion: {
+      done: false,
+      datetime: null
+    }
+  },
+  25: {
+    title: `REMINDER: How are your skills?`,
+    desc: `Remember to level your electronics skill mildly, and your charm (or) intimidate skill aggressively!`,
+    timeline: {
+      before: `Chapter 4: Noveria - Geth Interest`,
+      after: `Prologue`
+    },
+    completion: {
+      done: false,
+      datetime: null
+    }
+  },
+  30: {
+    title: `<b>Chapter 3</b>: Feros - Geth Attack`,
+    desc: `The geth have attacked the world of Feros. Your primary objective is to go to the Zhu's Hope colony to investigate what Saren is after, and why he sent his geth troops to the planet.`,
+    wiki: `http://masseffect.wikia.com/wiki/Feros:_Geth_Attack`,
+    timeline: {
+      before: `Feros: The Thorian`,
+      after: `Chapter 2: Finding Liara`
+    },
+    completion: {
+      done: false,
+      datetime: null
+    }
+  },
+  32: {
+    title: `HINT: Help around the colony`,
+    desc: `After clearing out the tower, ask Fai Dan how you can help around the colony, and complete those assignments <b>before</b> going up the elevator and completing Chapter 3: Feros - Geth Attack.`,
+    timeline: {
+      before: `Feros: The Thorian`,
+      after: `Chapter 1: Citadel - Expose Saren`
+    },
+    completion: {
+      done: false,
+      datetime: null
+    }
+  },
+  34: {
+    title: `HINT: Reserve grenades`,
+    desc: `Avoid using grenades at this stage. You will need them for Feros: The Thorian.`,
+    timeline: {
+      before: `Feros: The Thorian`,
+      after: `Chapter 1: Citadel - Expose Saren`
+    },
+    completion: {
+      done: false,
+      datetime: null
+    }
+  },
+  36: {
+    title: `Feros: The Thorian`,
+    desc: `Saren sent the geth to Feros to destroy an alien life form known as the Thorian. Why Saren wants this parasitic plant creature destroyed is still unclear, but it has evidently taken control of most of the colonists back at Zhu's Hope.`,
+    wiki: `http://masseffect.wikia.com/wiki/Feros:_The_Thorian`,
+    timeline: {
+      before: `Chapter 4: Noveria - Geth Interest`,
+      after: `Chapter 3: Feros - Geth Attack`
+    },
+    completion: {
+      done: false,
+      datetime: null
+    }
+  },
+  37: {
+    title: `HINT: Persuade Jeong`,
+    desc: `On the way back from ExoGeni HQ, when Lizbeth gets out of the Mako, follow her into the bunker. Assuming you've been aggressively levelling your charm (or) intimidate skills, you should be able to convince Jeong to stand down.<br /><br />
+      If you can't, and end up killing him, you will need to make sure you save as many colonists as possible, and let Shiala live (near the end of this mission).`,
+    timeline: {
+      before: `Feros: The Thorian (before you reach the colony)`,
+      after: `Feros: The Thorian (after leaving the HQ)`
+    },
+    completion: {
+      done: false,
+      datetime: null
+    }
+  },
+  38: {
+    title: `HINT: Equip Anti-Thorian Gas`,
+    desc: `After talking don Jeong, talk to Juliana. She will offer to give you the anti thorian gas. Equip it to your grenades from the inventory menu.<br /><br />
+      When you arrive at the colony, use the grenades on the colonists (don't waste them on the creatures) to disable then - without killing them.`,
+    timeline: {
+      before: `Feros: The Thorian (before you reach the colony)`,
+      after: `Feros: The Thorian (after leaving the bunker)`
+    },
+    completion: {
+      done: false,
+      datetime: null
+    }
+  },
+  39: {
+    title: `HINT: Colony success points`,
+    desc: `You definitely want the colony to succeed after you've left - so you need 13+ points from the following:<br /><br />
+      <ul>
+        <li><b>+1</b> point for each colony sidequest completed (varren meat, geth in the tunnels)</li>
+        <li><b>+1</b> point for each surviving colonist</li>
+        <li><b>+12</b> if Ethan Jeong agrees to help colony, OR <b>+5</b> if Shiala survives (only one or the other)</li>
+      </ul>`,
+    timeline: {
+      before: `Feros: The Thorian (completion)`,
+      after: `Chapter 3: Feros - Geth Attack`
+    },
+    completion: {
+      done: false,
+      datetime: null
+    }
+  },
 };
