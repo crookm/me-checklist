f9905c9ca663bb7f639f1ecef529a4f4fea39b18
Matt Crook <mcro591@aucklanduni.ac.nz>
2018-06-24 22:15:54 +1200

    move to component

--- src/_components/Checklist.js
+++ src/_components/Checklist.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 import "../_styles/Checklist.css";
+import HeroPrevNext from "./HeroPrevNext";
 
 class Checklist extends Component {
   constructor(props) {
@@ -311,78 +312,10 @@ class Checklist extends Component {
           ))}
 
           <div className="item">
-            <div className="row collapse align-center align-middle hero">
-              {this.props.gameMeta[this.props.game].prev && (
-                <div className="columns">
-                  <h5>Previously...</h5>
-                  <a
-                    href={
-                      this.props.gameMeta[
-                        this.props.gameMeta[this.props.game].prev
-                      ].link
-                    }
-                  >
-                    <div
-                      style={{
-                        background: `url("${
-                          this.props.gameMeta[
-                            this.props.gameMeta[this.props.game].prev
-                          ].bgimg
-                        }")`
-                      }}
-                    >
-                      {React.createElement(
-                        this.props.gameMeta[
-                          this.props.gameMeta[this.props.game].prev
-                        ].logo,
-                        {
-                          style: {
-                            width: "150px",
-                            margin: "0 auto",
-                            display: "block"
-                          }
-                        }
-                      )}
-                    </div>
-                  </a>
-                </div>
-              )}
-              {this.props.gameMeta[this.props.game].next && (
-                <div className="columns">
-                  <h5>Up next...</h5>
-                  <a
-                    href={
-                      this.props.gameMeta[
-                        this.props.gameMeta[this.props.game].next
-                      ].link
-                    }
-                  >
-                    <div
-                      style={{
-                        background: `url("${
-                          this.props.gameMeta[
-                            this.props.gameMeta[this.props.game].next
-                          ].bgimg
-                        }")`
-                      }}
-                    >
-                      {React.createElement(
-                        this.props.gameMeta[
-                          this.props.gameMeta[this.props.game].next
-                        ].logo,
-                        {
-                          style: {
-                            width: "150px",
-                            margin: "0 auto",
-                            display: "block"
-                          }
-                        }
-                      )}
-                    </div>
-                  </a>
-                </div>
-              )}
-            </div>
+            <HeroPrevNext
+              game={this.props.game}
+              gameMeta={this.props.gameMeta}
+            />
           </div>
         </div>
         <p style={{ margin: "1rem 0" }}>
