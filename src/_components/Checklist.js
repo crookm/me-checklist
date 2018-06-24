5dde96e0c04e3fe8045de51204ba9b0d64a7587f
Matt Crook <mcro591@aucklanduni.ac.nz>
2018-06-24 20:45:04 +1200

    sticky list menu

--- src/_components/Checklist.js
+++ src/_components/Checklist.js
@@ -37,6 +37,7 @@ class Checklist extends Component {
   componentDidMount() {
     window.$(".info").hide();
     window.$(document).foundation();
+    window.$(window).trigger("load.zf.sticky");
   }
 
   handleToggle(key, e) {
@@ -46,253 +47,269 @@ class Checklist extends Component {
 
   render() {
     return (
-      <div className="checklist">
-        <div className="row listmenu">
-          <div className="columns">
-            <p>
-              <Link to="/">&#x2190; game list</Link>
-            </p>
-          </div>
-          <div className="columns shrink">
-            <button
-              className="button small"
-              title="Expand all"
-              onClick={e => {
-                window
-                  .$(
-                    this.state.showSupplementary ? ".info" : ".info:not(.hint)"
-                  )
-                  .slideDown("fast")
-                  .parent()
-                  .addClass("active");
-                window.$(".carat").removeClass("down");
-              }}
-            >
-              <i className="material-icons">expand_more</i>
-            </button>
-            <button
-              className="button small"
-              title="Collapse all"
-              onClick={e => {
-                window
-                  .$(".info")
-                  .slideUp("fast")
-                  .parent()
-                  .removeClass("active");
-                window.$(".carat").addClass("down");
-              }}
-            >
-              <i className="material-icons">expand_less</i>
-            </button>
-          </div>
-          <div className="columns shrink">
-            <button
-              className="dropdown button small"
-              data-toggle="checklistSettings"
-            >
-              <i className="material-icons">settings</i>
-            </button>
-            <div
-              className="dropdown-pane"
-              data-position="bottom"
-              data-alignment="right"
-              id="checklistSettings"
-              data-dropdown
-              data-auto-focus="true"
-              data-close-on-click="true"
-            >
-              <label>
-                <input
-                  type="checkbox"
-                  checked={this.state.showSupplementary}
-                  onChange={e => {
-                    this.props.downstreamHandlers.handleSetUI(
-                      "showSupplementary",
-                      !this.state.showSupplementary
-                    );
-
-                    this.setState(prevState => ({
-                      showSupplementary: !prevState.showSupplementary
-                    }));
+      <div className="checklist" data-sticky-container>
+        <div style={{ height: "70px", display: "block" }}>
+          <div
+            className="sticky"
+            data-sticky
+            data-sticky-on="small"
+            data-anchor="list-actual"
+            style={{ width: "100%" }}
+          >
+            <div className="row listmenu">
+              <div className="columns">
+                <Link to="/" className="button small">
+                  &#x2190; game list
+                </Link>
+              </div>
+              <div className="columns shrink">
+                <button
+                  className="button small"
+                  title="Expand all"
+                  onClick={e => {
+                    window
+                      .$(
+                        this.state.showSupplementary
+                          ? ".info"
+                          : ".info:not(.hint)"
+                      )
+                      .slideDown("fast")
+                      .parent()
+                      .addClass("active");
+                    window.$(".carat").removeClass("down");
                   }}
-                />
-                Hints
-              </label>
-              <label>
-                <input
-                  type="checkbox"
-                  checked={this.state.showWikiLinks}
-                  onChange={e => {
-                    this.props.downstreamHandlers.handleSetUI(
-                      "showWikiLinks",
-                      !this.state.showWikiLinks
-                    );
-
-                    this.setState(prevState => ({
-                      showWikiLinks: !prevState.showWikiLinks
-                    }));
+                >
+                  <i className="material-icons">expand_more</i>
+                </button>
+                <button
+                  className="button small"
+                  title="Collapse all"
+                  onClick={e => {
+                    window
+                      .$(".info")
+                      .slideUp("fast")
+                      .parent()
+                      .removeClass("active");
+                    window.$(".carat").addClass("down");
                   }}
-                />
-                Wiki links
-              </label>
-              <label>
-                <input
-                  type="checkbox"
-                  checked={this.state.showTimelineSuggestions}
-                  onChange={e => {
-                    this.props.downstreamHandlers.handleSetUI(
-                      "showTimelineSuggestions",
-                      !this.state.showTimelineSuggestions
-                    );
+                >
+                  <i className="material-icons">expand_less</i>
+                </button>
+              </div>
+              <div className="columns shrink">
+                <button
+                  className="dropdown button small"
+                  data-toggle="checklistSettings"
+                >
+                  <i className="material-icons">settings</i>
+                </button>
+                <div
+                  className="dropdown-pane"
+                  data-position="bottom"
+                  data-alignment="right"
+                  id="checklistSettings"
+                  data-dropdown
+                  data-auto-focus="true"
+                  data-close-on-click="true"
+                >
+                  <label>
+                    <input
+                      type="checkbox"
+                      checked={this.state.showSupplementary}
+                      onChange={e => {
+                        this.props.downstreamHandlers.handleSetUI(
+                          "showSupplementary",
+                          !this.state.showSupplementary
+                        );
 
-                    this.setState(prevState => ({
-                      showTimelineSuggestions: !prevState.showTimelineSuggestions
-                    }));
-                  }}
-                />
-                Timeline suggestions
-              </label>
+                        this.setState(prevState => ({
+                          showSupplementary: !prevState.showSupplementary
+                        }));
+                      }}
+                    />
+                    Hints
+                  </label>
+                  <label>
+                    <input
+                      type="checkbox"
+                      checked={this.state.showWikiLinks}
+                      onChange={e => {
+                        this.props.downstreamHandlers.handleSetUI(
+                          "showWikiLinks",
+                          !this.state.showWikiLinks
+                        );
+
+                        this.setState(prevState => ({
+                          showWikiLinks: !prevState.showWikiLinks
+                        }));
+                      }}
+                    />
+                    Wiki links
+                  </label>
+                  <label>
+                    <input
+                      type="checkbox"
+                      checked={this.state.showTimelineSuggestions}
+                      onChange={e => {
+                        this.props.downstreamHandlers.handleSetUI(
+                          "showTimelineSuggestions",
+                          !this.state.showTimelineSuggestions
+                        );
+
+                        this.setState(prevState => ({
+                          showTimelineSuggestions: !prevState.showTimelineSuggestions
+                        }));
+                      }}
+                    />
+                    Timeline suggestions
+                  </label>
+                </div>
+              </div>
             </div>
           </div>
         </div>
 
-        {Object.entries(this.props.items).map(([key, entry]) => (
-          <div key={key} className={`item i-${key}`}>
-            {entry["wiki"] ? (
-              <div
-                className={
-                  /<[a-z][\s\S]*>/i.test(entry.title)
-                    ? "row head milestone"
-                    : "row head"
-                }
-                onClick={e => this.toggleCarat(key, e)}
-              >
-                <div className="columns shrink">
-                  <label
-                    className="checktainer"
-                    onClick={e => e.stopPropagation()}
-                  >
-                    <input
-                      type="checkbox"
-                      onChange={e => this.handleToggle(key, e)}
-                      checked={entry.completion.done ? true : false}
-                    />
-                    <span className="checkmark" />
-                  </label>
-                </div>
-                <div className="columns titlerow">
-                  <div className="row">
-                    <div className="columns title">
-                      <p>
-                        <a dangerouslySetInnerHTML={{ __html: entry.title }} />
-                      </p>
-                    </div>
-                    {this.state.showWikiLinks && (
-                      <div className="columns shrink">
-                        <p>
-                          <i
-                            className="material-icons"
-                            style={{ margin: "9px 0 10px" }}
-                          >
-                            <a
-                              href={entry.wiki}
-                              target="_BLANK"
-                              title="View on the Mass Effect Wiki"
-                              style={{
-                                color: "#d505ff"
-                              }}
-                              onClick={e => {
-                                this.props.downstreamHandlers.handleTrackOutboundLink(
-                                  e,
-                                  {
-                                    game: this.props.game,
-                                    linkSpecPurpose: "out to wikia",
-                                    linkVisualReferrer: `checklist item: ${entry.title.replace(
-                                      /<\/?[^>]+(>|$)/g,
-                                      ""
-                                    )}`,
-                                    linkVisualOrder: key
-                                  }
-                                );
-
-                                e.stopPropagation();
-                              }}
-                            >
-                              launch
-                            </a>
-                          </i>
-                        </p>
-                      </div>
-                    )}
-                  </div>
-                </div>
-                <div className="columns shrink">
-                  <a>
-                    <div className="carat up down" />
-                  </a>
-                </div>
-              </div>
-            ) : (
-              this.state.showSupplementary && (
-                // no wiki entry, so it's a hint
+        <div id="list-actual">
+          {Object.entries(this.props.items).map(([key, entry]) => (
+            <div key={key} className={`item i-${key}`}>
+              {entry["wiki"] ? (
                 <div
-                  className="row head"
-                  style={{
-                    padding: "0 10px",
-                    background: "#5f5f5f",
-                    borderBottom: "1px solid #444242"
-                  }}
+                  className={
+                    /<[a-z][\s\S]*>/i.test(entry.title)
+                      ? "row head milestone"
+                      : "row head"
+                  }
                   onClick={e => this.toggleCarat(key, e)}
                 >
                   <div className="columns shrink">
                     <label
                       className="checktainer"
-                      style={{ cursor: "unset" }}
-                    />
+                      onClick={e => e.stopPropagation()}
+                    >
+                      <input
+                        type="checkbox"
+                        onChange={e => this.handleToggle(key, e)}
+                        checked={entry.completion.done ? true : false}
+                      />
+                      <span className="checkmark" />
+                    </label>
                   </div>
-                  <div className="columns title">
-                    <p>
-                      <a style={{ color: "#f0f0f0" }}>{entry.title}</a>
-                    </p>
+                  <div className="columns titlerow">
+                    <div className="row">
+                      <div className="columns title">
+                        <p>
+                          <a
+                            dangerouslySetInnerHTML={{ __html: entry.title }}
+                          />
+                        </p>
+                      </div>
+                      {this.state.showWikiLinks && (
+                        <div className="columns shrink">
+                          <p>
+                            <i
+                              className="material-icons"
+                              style={{ margin: "9px 0 10px" }}
+                            >
+                              <a
+                                href={entry.wiki}
+                                target="_BLANK"
+                                title="View on the Mass Effect Wiki"
+                                style={{
+                                  color: "#d505ff"
+                                }}
+                                onClick={e => {
+                                  this.props.downstreamHandlers.handleTrackOutboundLink(
+                                    e,
+                                    {
+                                      game: this.props.game,
+                                      linkSpecPurpose: "out to wikia",
+                                      linkVisualReferrer: `checklist item: ${entry.title.replace(
+                                        /<\/?[^>]+(>|$)/g,
+                                        ""
+                                      )}`,
+                                      linkVisualOrder: key
+                                    }
+                                  );
+
+                                  e.stopPropagation();
+                                }}
+                              >
+                                launch
+                              </a>
+                            </i>
+                          </p>
+                        </div>
+                      )}
+                    </div>
                   </div>
                   <div className="columns shrink">
                     <a>
-                      <div className="carat down up" />
+                      <div className="carat up down" />
                     </a>
                   </div>
                 </div>
-              )
-            )}
-
-            <div className={`row info ${entry["wiki"] ? "" : "hint"}`}>
-              <div className="columns small-12">
-                <h3 dangerouslySetInnerHTML={{ __html: entry.title }} />
-                <p dangerouslySetInnerHTML={{ __html: entry.desc }} />
-
-                {this.state.showTimelineSuggestions && (
-                  <div className="row timeline">
-                    <div className="columns medium-6">
-                      <h4>Complete after</h4>
-                      <p
-                        dangerouslySetInnerHTML={{
-                          __html: entry.timeline.after
-                        }}
+              ) : (
+                this.state.showSupplementary && (
+                  // no wiki entry, so it's a hint
+                  <div
+                    className="row head"
+                    style={{
+                      padding: "0 10px",
+                      background: "#5f5f5f",
+                      borderBottom: "1px solid #444242"
+                    }}
+                    onClick={e => this.toggleCarat(key, e)}
+                  >
+                    <div className="columns shrink">
+                      <label
+                        className="checktainer"
+                        style={{ cursor: "unset" }}
                       />
                     </div>
-                    <div className="columns medium-6">
-                      <h4>Complete before</h4>
-                      <p
-                        dangerouslySetInnerHTML={{
-                          __html: entry.timeline.before
-                        }}
-                      />
+                    <div className="columns title">
+                      <p>
+                        <a style={{ color: "#f0f0f0" }}>{entry.title}</a>
+                      </p>
+                    </div>
+                    <div className="columns shrink">
+                      <a>
+                        <div className="carat down up" />
+                      </a>
                     </div>
                   </div>
-                )}
+                )
+              )}
+
+              <div className={`row info ${entry["wiki"] ? "" : "hint"}`}>
+                <div className="columns small-12">
+                  <h3 dangerouslySetInnerHTML={{ __html: entry.title }} />
+                  <p dangerouslySetInnerHTML={{ __html: entry.desc }} />
+
+                  {this.state.showTimelineSuggestions && (
+                    <div className="row timeline">
+                      <div className="columns medium-6">
+                        <h4>Complete after</h4>
+                        <p
+                          dangerouslySetInnerHTML={{
+                            __html: entry.timeline.after
+                          }}
+                        />
+                      </div>
+                      <div className="columns medium-6">
+                        <h4>Complete before</h4>
+                        <p
+                          dangerouslySetInnerHTML={{
+                            __html: entry.timeline.before
+                          }}
+                        />
+                      </div>
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
         <p style={{ margin: "1rem 0" }}>
           <Link to="/">&#x2190; return to game list</Link>
         </p>
